7e960eb417c8b22f27b27f67f141f7abbd0f7539
Thilagaraj <tk.thilagaraj@gmail.com>
2021-12-26 17:56:43 +0530

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -72,6 +72,17 @@
       var slides = document.querySelectorAll('.glides > div');
 
       var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
 function showSlides() {
   var i;
